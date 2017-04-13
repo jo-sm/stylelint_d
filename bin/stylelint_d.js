76d67b66f4ce6554da5075f19a5d4636d24083cc
Joshua Smock <joshuasmock@gmail.com>
2017-04-13 11:11:51 +0900

    Fix when parsing huge files or huge linter output

--- bin/stylelint_d.js
+++ bin/stylelint_d.js
@@ -2,12 +2,20 @@
 
 var minimist = require('minimist');
 var net = require('net');
-var msgpack = require('msgpack');
 var spawn = require('child_process').spawn;
 
 var packageJson = require('../package.json');
+var utils = require('../lib/utils');
+var separator = utils.separator;
+var generateError = utils.generateError;
 
 var args = minimist(process.argv.slice(2));
+
+function separateData(pieces) {
+  return pieces.join('').split(separator).filter(function(i) {
+    return Boolean(i.trim());
+  });
+}
 
 // If version, show current package.json version
 if (args.version || args.v) {
@@ -45,13 +53,14 @@ if (args.stdin) {
 
 function lint(args) {
   var command = args._[0];
+  var format = args.formatter;
 
   // Start or return server connection
   getServerConn().then(function(conn) {
     // If it's a start command, we handle it here
     if (command === 'start') {
       // The server has already started by virtue of getServerConn
-      console.log('stylelint_d started');
+      console.log('stylelint_d started.');
       conn.end();
       return;
     }
@@ -60,37 +69,85 @@ function lint(args) {
 
     // We write both the cwd and the potential filename, in
     // case we are given a relative path
-    conn.write(JSON.stringify([ process.cwd(), args ]) + "\r\n");
+    conn.write(JSON.stringify([ process.cwd(), args ]) + separator);
     conn.on('data', function(d) {
-      data.push(d);
+      data.push(d.toString('utf8'));
     });
 
     conn.on('end', function() {
-      var output;
+      data = separateData(data);
 
-      try {
-        output = msgpack.unpack(data.join(''));
-        output = output.output;
-      } catch(e) {
-        output = '';
+      // If we were given a command, first try to parse the
+      // resulting data in case it is a server message, and
+      // if it is, print that.
+      var message;
+
+      if (command) {
+        try {
+          message = JSON.parse(data.join(''));
+
+          if (message.message) {
+            console.log(message.message);
+            return;
+          }
+        } catch(e) {
+          console.log(e);
+        }
+
       }
 
-      console.log(output);
+      if (format === 'string') {
+        var result = data.join('');
+        console.log(result);
+      } else {
+        var parsedData = data.reduce(function(memo, i) {
+          if (i === 'stylelint_d: start') {
+            memo.start = true;
+          } else if (i === 'stylelint_d: deprecations') {
+            memo.currentType = 'deprecations';
+          } else if (i === 'stylelint_d: invalidOptionWarnings') {
+            memo.currentType = 'invalidOptionWarnings';
+          } else if (i === 'stylelint_d: warnings') {
+            memo.currentType = 'warnings';
+          } else if (memo.start) {
+            memo.start = false;
+            memo.currentIndex++;
+            memo.outputs.push({
+              source: i,
+              deprecations: [],
+              invalidOptionWarnings: [],
+              warnings: []
+            });
+          } else if (i) {
+            try {
+              i = JSON.parse(i);
+            } catch(e) {
+              throw new Error('Could not parse Stylelint JSON.');
+            }
+
+            memo.outputs[memo.currentIndex][memo.currentType].push(i);
+          }
+
+          return memo;
+        }, {
+          start: false,
+          currentIndex: -1,
+          currentType: 'deprecations',
+          outputs: []
+        });
+
+        console.log(JSON.stringify(parsedData.outputs));
+      }
     });
   }).catch(function(err) {
-    console.log(err);
+    console.log(generateError(err));
   });
 }
 
 function getServerConn() {
   var promise = new Promise(function(resolve, reject) {
     // Make sure the socket can be connected to
-    var socket = new net.Socket({
-      readable: true,
-      writeable: true,
-      allowHalfOpen: false,
-      fd: null
-    });
+    var socket = new net.Socket({});
 
     socket.once('error', function() {
       var server = require.resolve('../lib/server');
@@ -104,7 +161,9 @@ function getServerConn() {
       socket.destroy();
 
       // Wait for server to spawn and socket to connect
-      waitForSocket().then(resolve).catch(reject);
+      waitForSocket().then(function(actualSocket) {
+        resolve(actualSocket);
+      }).catch(reject);
     });
 
     socket.once('connect', function() {
@@ -120,12 +179,7 @@ function getServerConn() {
 function waitForSocket() {
   return new Promise(function(resolve) {
     var wait = function() {
-      var socket = new net.Socket({
-        readable: true,
-        writeable: true,
-        allowHalfOpen: false,
-        fd: null
-      });
+      var socket = new net.Socket({});
 
       socket.once('error', wait);
 
