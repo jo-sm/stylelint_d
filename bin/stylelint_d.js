7122dbc635463da78cbc9b6006e786a48c0cec4c
Joshua Smock <jo-sm@users.noreply.github.com>
2020-12-22 22:38:17 +0100

    Format all files with Prettier

--- bin/stylelint_d.js
+++ bin/stylelint_d.js
@@ -7,12 +7,12 @@
   2: At least one rule with an "error"-level severity triggered at least one violations.
  */
 
-var minimist = require('minimist');
-var net = require('net');
-var spawn = require('child_process').spawn;
+var minimist = require("minimist");
+var net = require("net");
+var spawn = require("child_process").spawn;
 
-var packageJson = require('../package.json');
-var utils = require('../lib/utils');
+var packageJson = require("../package.json");
+var utils = require("../lib/utils");
 var separator = utils.separator;
 var generateError = utils.generateError;
 var validCommand = utils.validCommand;
@@ -20,9 +20,12 @@ var validCommand = utils.validCommand;
 var argv = minimist(process.argv.slice(2));
 
 function separateData(pieces) {
-  return pieces.join('').split(separator).filter(function(i) {
-    return Boolean(i.trim());
-  });
+  return pieces
+    .join("")
+    .split(separator)
+    .filter(function (i) {
+      return Boolean(i.trim());
+    });
 }
 
 // If version, show current package.json version
@@ -31,18 +34,18 @@ if (argv.version || argv.v) {
   return;
 }
 
-var stdin = '';
+var stdin = "";
 var filename = argv.file || argv.f;
 var config = argv.config || argv.c;
 var language = argv.language;
 
 if (!argv.formatter) {
-  argv.formatter = 'string';
+  argv.formatter = "string";
 }
 
 if (argv.stdin) {
   if (!filename && !config) {
-    console.log('Error: --stdin requires --config or --file flags');
+    console.log("Error: --stdin requires --config or --file flags");
     return;
   }
 
@@ -50,15 +53,19 @@ if (argv.stdin) {
   var recommendedSyntaxes = "scss, less, sugarss";
 
   if (language && validSyntaxes.indexOf(language) === -1) {
-    console.log(`Error: invalid language (${language}). Valid languages: ${recommendedSyntaxes}`);
+    console.log(
+      `Error: invalid language (${language}). Valid languages: ${recommendedSyntaxes}`
+    );
     return;
   }
 
   if (language && filename) {
-    console.log(`Notice: --language has higher precedence than the extension in the filename.\n${filename} will be parsed with syntax ${language}.`);
+    console.log(
+      `Notice: --language has higher precedence than the extension in the filename.\n${filename} will be parsed with syntax ${language}.`
+    );
   }
 
-  process.stdin.on('readable', function() {
+  process.stdin.on("readable", function () {
     var chunk = process.stdin.read();
 
     if (chunk !== null) {
@@ -66,9 +73,9 @@ if (argv.stdin) {
     }
   });
 
-  process.stdin.on('end', function() {
+  process.stdin.on("end", function () {
     argv.stdin = stdin;
-    argv.files = [ filename ];
+    argv.files = [filename];
     lint(argv);
   });
 } else {
@@ -81,157 +88,161 @@ function lint(args) {
   var format = args.formatter;
 
   // Start or return server connection
-  getServerConn().then(function(conn) {
-    // If it's a start command, we handle it here
-    if (command === 'start') {
-      // The server has already started by virtue of getServerConn
-      console.log('stylelint_d started.');
-      conn.end();
-      return;
-    }
+  getServerConn()
+    .then(function (conn) {
+      // If it's a start command, we handle it here
+      if (command === "start") {
+        // The server has already started by virtue of getServerConn
+        console.log("stylelint_d started.");
+        conn.end();
+        return;
+      }
 
-    var data = [];
+      var data = [];
 
-    // We write both the cwd and the potential filename, in
-    // case we are given a relative path
-    conn.write(JSON.stringify([ process.cwd(), args ]) + separator);
-    conn.on('data', function(d) {
-      data.push(d.toString('utf8'));
-    });
+      // We write both the cwd and the potential filename, in
+      // case we are given a relative path
+      conn.write(JSON.stringify([process.cwd(), args]) + separator);
+      conn.on("data", function (d) {
+        data.push(d.toString("utf8"));
+      });
 
-    conn.on('end', function() {
-      data = separateData(data);
+      conn.on("end", function () {
+        data = separateData(data);
 
-      // If we were given a command, first try to parse the
-      // resulting data in case it is a server message, and
-      // if it is, print that.
-      var message;
+        // If we were given a command, first try to parse the
+        // resulting data in case it is a server message, and
+        // if it is, print that.
+        var message;
 
-      if (validCommand(command)) {
-        try {
-          message = JSON.parse(data.join(''));
+        if (validCommand(command)) {
+          try {
+            message = JSON.parse(data.join(""));
 
-          if (message.message) {
-            console.log(message.message);
+            if (message.message) {
+              console.log(message.message);
+            }
+          } catch (e) {
+            process.exitCode = 1;
+
+            console.log("Could not parse JSON after ended:");
+            console.log(e);
           }
-        } catch(e) {
-
-          process.exitCode = 1;
-
-          console.log('Could not parse JSON after ended:');
-          console.log(e);
-        }
-
-        return;
-      }
-
-      // Determine if it's an error, first
-      var errorObject;
-      var parsed;
-      if (data[0] === 'stylelint_d: isError') {
-        errorObject = data[1];
-
-        try {
-          parsed = JSON.parse(errorObject);
-        } catch(e) {
-          throw new Error('Unknown error occurred.');
-        }
-
-        process.exitCode = parsed.code;
-
-        console.log(parsed.message);
-
-        return;
-      }
-
-      if (format === 'string') {
-        var result = data.join('');
-
-        try {
-          parsed = JSON.parse(result);
-        } catch(e) {
-          process.exitCode = 1;
-
-          console.log('Error: Could not parse `stylelint_d` result');
-          console.error(e);
 
           return;
         }
 
-        var didErrored = parsed.errored;
-        var output = parsed.output;
+        // Determine if it's an error, first
+        var errorObject;
+        var parsed;
+        if (data[0] === "stylelint_d: isError") {
+          errorObject = data[1];
 
-        if (didErrored) {
-          process.exitCode = 2;
-        }
-
-        console.log(output);
-      } else {
-        var parsedData = data.reduce(function(memo, i) {
-          if (i === 'stylelint_d: start') {
-            memo.start = true;
-          } else if (i === 'stylelint_d: deprecations') {
-            memo.currentType = 'deprecations';
-          } else if (i === 'stylelint_d: invalidOptionWarnings') {
-            memo.currentType = 'invalidOptionWarnings';
-          } else if (i === 'stylelint_d: warnings') {
-            memo.currentType = 'warnings';
-          } else if (i === 'stylelint_d: exitCode') {
-            memo.currentType = 'exitCode';
-          } else if (memo.start) {
-            memo.start = false;
-            memo.currentIndex++;
-            memo.outputs.push({
-              source: i,
-              deprecations: [],
-              invalidOptionWarnings: [],
-              warnings: []
-            });
-          } else if (i) {
-            try {
-              i = JSON.parse(i);
-            } catch(e) {
-              process.exitCode = 1;
-
-              console.log(i);
-              throw new Error('Could not parse Stylelint JSON.');
-            }
-
-            if (memo.currentType === 'exitCode') {
-              process.exitCode = i;
-            } else {
-              memo.outputs[memo.currentIndex][memo.currentType].push(i);
-            }
+          try {
+            parsed = JSON.parse(errorObject);
+          } catch (e) {
+            throw new Error("Unknown error occurred.");
           }
 
-          return memo;
-        }, {
-          start: false,
-          currentIndex: -1,
-          currentType: 'deprecations',
-          outputs: []
-        });
+          process.exitCode = parsed.code;
 
-        console.log(JSON.stringify(parsedData.outputs));
-      }
+          console.log(parsed.message);
+
+          return;
+        }
+
+        if (format === "string") {
+          var result = data.join("");
+
+          try {
+            parsed = JSON.parse(result);
+          } catch (e) {
+            process.exitCode = 1;
+
+            console.log("Error: Could not parse `stylelint_d` result");
+            console.error(e);
+
+            return;
+          }
+
+          var didErrored = parsed.errored;
+          var output = parsed.output;
+
+          if (didErrored) {
+            process.exitCode = 2;
+          }
+
+          console.log(output);
+        } else {
+          var parsedData = data.reduce(
+            function (memo, i) {
+              if (i === "stylelint_d: start") {
+                memo.start = true;
+              } else if (i === "stylelint_d: deprecations") {
+                memo.currentType = "deprecations";
+              } else if (i === "stylelint_d: invalidOptionWarnings") {
+                memo.currentType = "invalidOptionWarnings";
+              } else if (i === "stylelint_d: warnings") {
+                memo.currentType = "warnings";
+              } else if (i === "stylelint_d: exitCode") {
+                memo.currentType = "exitCode";
+              } else if (memo.start) {
+                memo.start = false;
+                memo.currentIndex++;
+                memo.outputs.push({
+                  source: i,
+                  deprecations: [],
+                  invalidOptionWarnings: [],
+                  warnings: [],
+                });
+              } else if (i) {
+                try {
+                  i = JSON.parse(i);
+                } catch (e) {
+                  process.exitCode = 1;
+
+                  console.log(i);
+                  throw new Error("Could not parse Stylelint JSON.");
+                }
+
+                if (memo.currentType === "exitCode") {
+                  process.exitCode = i;
+                } else {
+                  memo.outputs[memo.currentIndex][memo.currentType].push(i);
+                }
+              }
+
+              return memo;
+            },
+            {
+              start: false,
+              currentIndex: -1,
+              currentType: "deprecations",
+              outputs: [],
+            }
+          );
+
+          console.log(JSON.stringify(parsedData.outputs));
+        }
+      });
+    })
+    .catch(function (err) {
+      process.exitCode = 1;
+
+      console.log(generateError(err));
     });
-  }).catch(function(err) {
-    process.exitCode = 1;
-
-    console.log(generateError(err));
-  });
 }
 
 function getServerConn() {
-  var promise = new Promise(function(resolve, reject) {
+  var promise = new Promise(function (resolve, reject) {
     // Make sure the socket can be connected to
     var socket = new net.Socket({});
 
-    socket.once('error', function() {
-      var server = require.resolve('../lib/server');
-      var child = spawn('node', [ server ], {
+    socket.once("error", function () {
+      var server = require.resolve("../lib/server");
+      var child = spawn("node", [server], {
         detached: true,
-        stdio: [ 'ignore', 'ignore', 'ignore' ],
+        stdio: ["ignore", "ignore", "ignore"],
       });
       child.unref();
 
@@ -239,33 +250,35 @@ function getServerConn() {
       socket.destroy();
 
       // Wait for server to spawn and socket to connect
-      waitForSocket().then(function(actualSocket) {
-        resolve(actualSocket);
-      }).catch(reject);
+      waitForSocket()
+        .then(function (actualSocket) {
+          resolve(actualSocket);
+        })
+        .catch(reject);
     });
 
-    socket.once('connect', function() {
+    socket.once("connect", function () {
       resolve(socket);
     });
 
-    socket.connect({ port: 48126, host: '127.0.0.1' });
+    socket.connect({ port: 48126, host: "127.0.0.1" });
   });
 
   return promise;
 }
 
 function waitForSocket() {
-  return new Promise(function(resolve) {
-    var wait = function() {
+  return new Promise(function (resolve) {
+    var wait = function () {
       var socket = new net.Socket({});
 
-      socket.once('error', wait);
+      socket.once("error", wait);
 
-      socket.once('connect', function() {
+      socket.once("connect", function () {
         resolve(socket);
       });
 
-      socket.connect({ port: 48126, host: '127.0.0.1' });
+      socket.connect({ port: 48126, host: "127.0.0.1" });
     };
 
     wait();
