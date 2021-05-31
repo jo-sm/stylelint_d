4f7b632a9717f18e61913f91cb2d04cb0cb80630
Joshua Smock <jo-sm@users.noreply.github.com>
2021-06-01 00:14:25 +0200

    Add basic stylelint integration tests
    Just to ensure that a major version update doesn't fundamentally break things
    
    - Add eslint-plugin-jest and fix errors and warnings

--- src/Defer.spec.ts
+++ src/Defer.spec.ts
@@ -1,20 +1,20 @@
 import { Defer } from "./Defer";
 
 describe("Defer", () => {
-  it("should resolve the promise once the instance resolve method is called", () => {
+  it("should resolve the promise once the instance resolve method is called", async () => {
     const defer = new Defer<string>();
 
     defer.resolve("success");
 
-    expect(defer.promise).resolves.toBe("success");
+    await expect(defer.promise).resolves.toBe("success");
   });
 
-  it("should reject the promise once the instance reject method is called", () => {
+  it("should reject the promise once the instance reject method is called", async () => {
     const err = new Error("oops");
     const defer = new Defer<string>();
 
     defer.reject(err);
 
-    expect(defer.promise).rejects.toBe(err);
+    await expect(defer.promise).rejects.toBe(err);
   });
 });
